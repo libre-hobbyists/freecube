57d33539aa4f3f865c5c5a1232826b98e0c151b7
samdoesnerdstuff <embeddednerdgirl@gmail.com>
2025-12-09 08:36:39 -0330

    Refactor wWinMain to allow for command-line args

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,6 @@
         #define WIN32_LEAN_AND_MEAN
     #endif
     #include <Windows.h>
-
-    int main();
-
-    int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
-        main();
-    }
 #endif
 
 #include "util/log.hpp"
@@ -16,23 +10,58 @@
 #include "dol/dol_loader.hpp"
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <cstring>
 
-int main() {
+int main(int argc, char **argv) {
     using namespace freecube::ISOLoader;
 
-    ISOImage iso("loz_ww.iso");
+    std::string iso_path;
 
-    auto dol = iso.extract_file("main.dol");
-    iso.dump_fst();
+    for (int i = 1; i < argc; i++) {
+        std::string arg = argv[i];
+        if (arg.rfind("--iso=", 0) == 0) {
+            iso_path = arg.substr(6);
+        } else if (arg == "--iso" && i + 1 < argc) {
+            // Next arg is the path
+            iso_path = argv[++i];
+        }
+    }
 
-    if (!dol) {
-        LOG_CRITICAL("main.dol not found!");
+    if (iso_path.empty()) {
+        LOG_CRITICAL("No ISO file specified!");
+        LOG_INFO("Use: freecube --iso=\"path/to/data.iso\"");
         return -1;
     }
 
-    freecube::dol::DOLLoader loader(*dol);
-
-    LOG_INFO("Loaded DOL entrypoint => ", loader.image().entry_point);
+    ISOImage iso(iso_path);
 
     return 0;
 }
+
+#if defined(_WIN32) || defined(_WIN64)
+    // Safe conversion of Windows args to more nix-like args
+    int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
+        int argc;
+
+        LPWSTR *wargv = CommandLineToArgvW(GetCommandLineW(), &argc);
+
+        char **argv = new char*[argc];
+        for (int i = 0; i < argc; i++) {
+            int size = WideCharToMultiByte(CP_UTF8, 0, wargv[i], -1, nullptr, 0, nullptr, nullptr);
+            argv[i] = new char[size];
+            WideCharToMultiByte(CP_UTF8, 0, wargv[i], -1, argv[i], size, nullptr, nullptr);
+        }
+
+        int result = main(argc, argv);
+
+        // Cleanup our argv mess
+        for (int i = 0; i < argc; i++) {
+            delete[] argv[i];
+        }
+        delete[] argv;
+        LocalFree(wargv);
+        
+        return result;
+    }
+#endif
